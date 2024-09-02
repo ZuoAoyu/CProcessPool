59d31a85bef6a5d10ed9bec8e16f760b9aa79f1e
aoyu <aoyu@xiake.me>
2024-09-02 14:29:23 +0800

    使用管道通知工作进程终止

--- main.c
+++ main.c
@@ -3,24 +3,17 @@
  */
 #include "head.h"
 
-// workerList 访问所有工作进程的状态 改为全局变量
-processData_t *workerList;
-
-// 工作进程的数量 改为全局变量
-int workerNum;
+// 保存匿名管道两端的描述符，
+// 索引 0 是读端的文件描述符，索引 1 是写端的文件描述符
+int pipeFd[2];
 
 void sigFunc(int signum) {
   printf("signum = %d\n", signum);
-  for (int i = 0; i < workerNum; ++i) {
-    kill(workerList[i].pid, SIGKILL);
-  }
 
-  for (int i = 0; i < workerNum; ++i) {
-    wait(NULL);
-  }
-
-  puts("process pool is over!");
-  exit(0);
+  // 向管道写端写入数据
+  write(pipeFd[1], "1", 1);
+  // 发送什么无所谓，只要发过去了，管道读端就绪了，就知道程序该退出了
+  // ssize_t write(int fd, const void *buf, size_t count);
 }
 
 int main(int argc, char *argv[]) {
@@ -30,11 +23,15 @@ int main(int argc, char *argv[]) {
     exit(-1);
   }
 
+  // 创建匿名管道
+  pipe(pipeFd);
+
   // 工作进程的数量
-  workerNum = atoi(argv[3]);
+  int workerNum = atoi(argv[3]);
 
   // workerList 访问所有工作进程的状态 改为全局变量
-  workerList = (processData_t *)calloc(sizeof(processData_t), workerNum);
+  processData_t *workerList =
+      (processData_t *)calloc(sizeof(processData_t), workerNum);
 
   // 注册信号处理函数
   signal(SIGUSR1, sigFunc);
@@ -52,6 +49,9 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < workerNum; ++i) {
     epollAdd(workerList[i].pipeFd, epfd);
   }
+
+  // 将 管道读端 添加到 epoll 实例中进行监控
+  epollAdd(pipeFd[0], epfd);
 
   int listenSize = workerNum + 1; // 监听的 fd 的数量
   // （1 个 socket 和 每个进程的 本地套接字 的一端（只用来读））
@@ -77,6 +77,18 @@ int main(int argc, char *argv[]) {
         }
 
         close(netFd); // 父进程把 netFd 的处理交给了子进程，所以父进程关闭套接字
+      } else if (readyList[i].data.fd == pipeFd[0]) {
+        for (int i = 0; i < workerNum; ++i) {
+          printf("send signal to worker %d.\n", workerList[i].pid);
+          kill(workerList[i].pid, SIGKILL);
+        }
+
+        for (int i = 0; i < workerNum; ++i) {
+          wait(NULL);
+        }
+
+        puts("process pool is over!");
+        exit(0);
       } else {
         puts("one worker finish its task");
 
