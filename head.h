a1a07d2a797a7eb7033d9ca68e918ce252e74b5b
aoyu <aoyu@xiake.me>
2024-09-02 10:21:22 +0800

    mmap文件映射减少 read，分批发送 和 一次性接收

--- head.h
+++ head.h
@@ -7,7 +7,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/epoll.h>
+#include <sys/mman.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
--- transFile.c
+++ transFile.c
@@ -54,29 +54,30 @@ int recvFile(int netFd) {
   // 先创建文件
   char filepath[100] = "temp/";
   strcat(filepath, t.buf);
-  int fd = open(filepath, O_WRONLY | O_CREAT, 0666);
+  int fd = open(filepath, O_RDWR | O_CREAT, 0666); // 由 O_WRONLY 改为 O_RDWR
   ERROR_CHECK(fd, -1, "open");
 
-  int ret;
-  while (1) {
-    // 接收 文件内容长度 和 文件内容
-    bzero(&t, sizeof(t));
-    recvn(netFd, &t.dataLength, sizeof(int));
-    if (t.dataLength == 0) {
-      break;
-    }
+  // 接收文件总大小
+  off_t fileSize;
+  bzero(&t, sizeof(t));
+  recvn(netFd, &t.dataLength, sizeof(int));
+  recvn(netFd, &fileSize, t.dataLength);
+  printf("fileSize = %ld\n", fileSize);
 
-    /* ret = recv(netFd, t.buf, t.dataLength, 0); */
-    /* ERROR_CHECK(ret, -1, "recv"); */
-    // 调用recv 时，传入的整型长度参数（这里是t.dataLength）描述的是最大长度，
-    // 而不是本次 recv 所能获取到的数据长度。
-    // 实际 recv 接收到的数据长度可能无法达到这个长度，
-    // 因为TCP是一种流式协议，它只能负责每个报文可靠有序地发送和接收，
-    // 但是并不能保证传输到网络缓冲区当中的是完整的一个"小火车"。
-    recvn(netFd, t.buf, t.dataLength);
+  // 调整文件大小到指定大小
+  ftruncate(fd, fileSize);
 
-    write(fd, t.buf, t.dataLength);
-  }
+  // 建立磁盘文件和用户态内存之间的映射
+  // 因为后面需要读写 mmap 映射的数据，所以前面 open 时的权限改为 O_RDWR
+  char *p =
+      (char *)mmap(NULL, fileSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  ERROR_CHECK(p, MAP_FAILED, "mmap");
+
+  // 接收文件内容，一次性接收完
+  recvn(netFd, p, fileSize);
+
+  // 解除映射
+  munmap(p, fileSize);
 
   // 关闭打开的文件的 文件描述符
   close(fd);
@@ -93,50 +94,48 @@ int sendFile(int netFd) {
   int fd = open(t.buf, O_RDONLY);
   ERROR_CHECK(fd, -1, "open");
 
-  // 文件总大小
-  off_t fileSize = getFileSize(fd);
-  // 已发送总大小
-  off_t doneSize = 0;
-  // 上次已发送总大小
-  off_t lastSize = 0;
-  // 每个切片的文件大小
-  off_t slice = fileSize / 100;
+  // 文件状态信息
+  struct stat statbuf;
+  int ret = fstat(fd, &statbuf);
 
-  while (1) {
-    // 读取文件内容
+  // 发送文件大小
+  bzero(&t, sizeof(t));
+  t.dataLength = sizeof(statbuf.st_size);
+  memcpy(t.buf, &statbuf.st_size, t.dataLength);
+  send(netFd, &t, sizeof(int) + sizeof(off_t), MSG_NOSIGNAL);
+
+  // 建立磁盘文件和用户态内存之间的映射
+  char *p = (char *)mmap(NULL, statbuf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+  ERROR_CHECK(p, MAP_FAILED, "mmap");
+
+  off_t total = 0;
+  while (total < statbuf.st_size) {
     bzero(&t, sizeof(t));
-    int ret = read(fd, t.buf, sizeof(t.buf));
-    ERROR_CHECK(ret, -1, "read");
-    t.dataLength = ret;
 
-    if (t.dataLength != sizeof(t.buf)) {
-      // 要发送的内容未占满 t.buf
-      printf("dataLength = %d\n", t.dataLength);
+    // “小火车”每次运多少数据
+    if (statbuf.st_size - total > sizeof(t.buf)) {
+      t.dataLength = sizeof(t.buf);
+    } else {
+      t.dataLength = statbuf.st_size - total;
     }
 
-    if (t.dataLength == 0) {
+    memcpy(t.buf, p + total, t.dataLength);
+    total += t.dataLength;
+
+    // 发送数据
+    // 注意只发送缓冲区，因客户端是一次性接收
+    ret = send(netFd, &t.buf, t.dataLength, MSG_NOSIGNAL);
+    if (ret == -1) {
+      perror("send");
       break;
-    }
-
-    // 发送 文件内容长度 和 文件内容
-    ret = send(netFd, &t, sizeof(int) + t.dataLength, MSG_NOSIGNAL);
-    ERROR_CHECK(ret, -1, "send");
-
-    doneSize += t.dataLength;
-    if (doneSize - lastSize >= slice) {
-      printf("%5.2lf%%\r", 100.0 * doneSize / fileSize);
-      fflush(stdout); // 清空缓冲区
-      lastSize = doneSize;
     }
   }
 
-  // 结束的时候发送一个车厢为 0 的小火车
+  // 结束的时候发送一个车厢为 0 的小火车（结束标志）
   t.dataLength = 0;
   send(netFd, &t, sizeof(int), MSG_NOSIGNAL);
 
-  printf("100.00%%\n");
-
+  munmap(p, statbuf.st_size);
   close(fd);
-
   return 0;
 }
